6e28f2786bd6b5f11bb3a306deeebd49d5d795cc
xandevmferreira <alexandrevmferreira@gmail.com>
2024-08-04 07:23:49 -0300

    https://github.com/xandevmferreira/daily-boat-dash.git

---
+++ codigo.py
@@ -0,0 +1,60 @@
+from dash import Dash, html, dcc
+
+app = Dash('daily boat')
+
+app.layout = html.Div([
+    html.Div(children=[
+        html.Label('Paciente'),
+        dcc.Dropdown(['Fulando da Silva', 'Siclano Ferreira', 'Raimundo Nonato'], 'Fulano da Silva'),
+
+        html.Br(),
+        html.Label('Saúde mental'),
+        dcc.Dropdown(['Depressão', 'Autismo', 'TDAH'],
+                     [],
+                     multi=True),
+
+        html.Br(),
+        html.Label('Nota Fiscal'),
+        dcc.Dropdown(['Sim', 'Não'],
+                     [],
+                     multi=True),
+
+        html.Br(),
+        html.Label('Método de pagamento'),
+        dcc.Dropdown(['Débito', 'Crédito', 'Mensal', 'Por consulta'],
+                     [],
+                     multi=True),
+
+        html.Br(),
+        html.Label('Prioridade'),
+        dcc.RadioItems(['Baixa', 'Média', 'Baixa'], ''),
+    ], style={'padding': 10, 'flex': 1}),
+
+        html.Br(),
+        html.Label('Dia da consulta'),
+        dcc.Checklist(['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta'],
+    style={'padding': 10, 'flex': 1}),
+
+        html.Div(children=[
+        html.Label('Flexbilidade de horário'),
+        dcc.Checklist(['Sim', 'Não'],
+                      ['']
+        ),
+
+        html.Br(),
+        html.Label('Observações'),
+        dcc.Input(value='anotações', type='text'),
+
+        html.Br(),
+        html.Label('Horário atendimento'),
+        dcc.Slider(
+            min=8,
+            max=22,
+            marks={i: f'Label {i}' if i == 1 else str(i) for i in range(1, 24)},
+            value=0,
+        ),
+    ], style={'padding': 10, 'flex': 1})
+], style={'display': 'flex', 'flexDirection': 'row'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
